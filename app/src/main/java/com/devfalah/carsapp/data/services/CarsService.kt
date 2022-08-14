c36723ed7e6248baf88474d6b7fa6cd39b3f623d
Falah Hassan <devfalahhassan@gmail.com>
2022-08-14 10:09:17 -0700

    add car service

---
+++ app/src/main/java/com/devfalah/carsapp/data/services/CarsService.kt
@@ -0,0 +1,26 @@
+package com.devfalah.carsapp.data.services
+
+import android.util.Log
+import com.devfalah.carsapp.data.State
+import com.devfalah.carsapp.data.models.Car
+import com.devfalah.carsapp.data.models.CarsModel
+import okhttp3.OkHttpClient
+
+interface BaseCarsService {
+    fun getCars(): State<List<Car>>
+}
+
+object CarsService : BaseCarsService {
+    private val client = OkHttpClient()
+    override fun getCars():State<List<Car>> {
+        val response = client.newCall(RequestBuilder.makeCarsRequest()).execute()
+        return if (response.isSuccessful) {
+            CarsModel().fromJson(response.body!!.string())
+                .run {
+                State.Success(this)
+            }
+        } else {
+            State.Fail(response.message)
+        }
+    }
+}
